6bca6d16db308e526df1daef628f53a0a9d5bad8
Jason Stocker <jasonstocker@Jasons-MacBook-Pro.local>
2018-08-12 17:15:02 -0400

    Created inital navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import NavBar from './components/NavBar'
+import Navigation from './components/Navigation'
 class App extends Component {
     render() {
         return (
             <div>
-                <NavBar />
+                <Navigation />
             </div>
         )
     }
