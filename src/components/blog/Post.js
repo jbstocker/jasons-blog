3164c3700c667a6a22c5f556c02f1b677e4b2eba
Jason Stocker <jbstocker32@gmail.com>
2021-12-26 18:42:00 -0500

    Attempt to fix issue with deployment

--- src/components/blog/Post.js
+++ src/components/blog/Post.js
@@ -4,7 +4,7 @@ import Copyright from "../copyright/Copyright";
 import Contact from "../contact/Contact";
 import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
-import Prism from "prismjs";
+// import Prism from "prismjs";
 import './Post.css'
 import './prism.css'
 
@@ -16,9 +16,9 @@ function Post(props) {
   const [post, setPost] = useState('');
   const [coverImage, setCoverImage] = useState('');
 
-  useEffect(() => {
-    Prism.highlightAll();
-  });
+  // useEffect(() => {
+  //   Prism.highlightAll();
+  // });
 
   useEffect(() => {
     import(`./posts/${props.postData.contentFile}`)
