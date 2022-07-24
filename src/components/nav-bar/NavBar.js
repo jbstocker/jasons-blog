0583be0c28bee6e083a53cca77d50beab1f01fe3
Jason Stocker <jbstocker32@gmail.com>
2022-07-24 17:13:21 -0400

    Constant size on navbar

--- src/components/nav-bar/NavBar.js
+++ src/components/nav-bar/NavBar.js
@@ -1,24 +1,24 @@
 import './NavBar.css';
-import { useNavigate } from "react-router-dom";
-import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+// import { useState } from 'react';
 
 function NavBar(props) {
   const navigate = useNavigate();
   const toBlog = () => {
     scrollToTop();
     navigate('/blog');
-  }
-  window.onscroll = () => {
-    setTimeout(() => {
-      if (window.pageYOffset < 5) {
-        setExpanded('navbar expanded');
-      } else {
-        setExpanded('navbar collapsed');
-      }
-    }, 0);
   };
+  // window.onscroll = () => {
+  //   setTimeout(() => {
+  //     if (window.pageYOffset < 5) {
+  //       setExpanded('navbar expanded');
+  //     } else {
+  //       setExpanded('navbar collapsed');
+  //     }
+  //   }, 0);
+  // };
 
-  const [expanded, setExpanded] = useState('navbar expanded');
+  // const [expanded, setExpanded] = useState('navbar expanded');
 
   const externalNavLinks = {
     github: 'https://github.com/jbstocker',
@@ -33,18 +33,32 @@ function NavBar(props) {
   };
 
   return (
-    <div className={`${expanded}`}>
-      <div className="title">
-        <i className="fas fa-code" onClick={scrollToTop}></i>
+    <div className={'navbar expanded'}>
+      <div className='title'>
+        <i className='fas fa-code' onClick={scrollToTop}></i>
         <h1 onClick={scrollToTop}>{props.title}</h1>
       </div>
-      <div className="linkbar">
-        <p className="home" onClick={props.scrollToAbout}>home</p>
+      <div className='linkbar'>
+        <p className='home' onClick={props.scrollToAbout}>
+          home
+        </p>
         <p onClick={toBlog}>blog</p>
-        <a className="externalNav" href={externalNavLinks.github} title="Github" target="_blank" rel="noreferrer">
+        <a
+          className='externalNav'
+          href={externalNavLinks.github}
+          title='Github'
+          target='_blank'
+          rel='noreferrer'
+        >
           <p>github</p>
         </a>
-        <a className="externalNav" href={externalNavLinks.linkedIn} title="LinkedIn" target="_blank" rel="noreferrer">
+        <a
+          className='externalNav'
+          href={externalNavLinks.linkedIn}
+          title='LinkedIn'
+          target='_blank'
+          rel='noreferrer'
+        >
           <p>linkedIn</p>
         </a>
       </div>
