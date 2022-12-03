e3c74656d327d27cfc2338ff6a5451c98020c8a8
Jason Stocker <jbstocker32@gmail.com>
2022-12-02 20:47:50 -0500

    feat: Add animate on scroll

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,29 +7,40 @@ import Contact from '../contact/Contact';
 import Copyright from '../copyright/Copyright';
 
 function Home() {
-
   const aboutRef = useRef();
   const projectsRef = useRef();
   const contactRef = useRef();
 
   function scrollToAbout() {
-    aboutRef.current.scrollIntoView({ behavior: 'smooth', block: "start", inline: "nearest" })
+    aboutRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest'
+    });
   }
 
   function scrollToProjects() {
-    projectsRef.current.scrollIntoView({ behavior: 'smooth', block: "start", inline: "nearest" })
+    projectsRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest'
+    });
   }
 
   return (
     <div>
-      <NavBar title='JasonStocker.com' scrollToAbout={scrollToAbout} scrollToProjects={scrollToProjects} />
-      <div className="section" ref={aboutRef}>
+      <NavBar
+        title='JasonStocker.com'
+        scrollToAbout={scrollToAbout}
+        scrollToProjects={scrollToProjects}
+      />
+      <div className='section' ref={aboutRef}>
         <About />
       </div>
-      <div className="section" ref={projectsRef}>
+      <div className='section' ref={projectsRef}>
         <Projects />
       </div>
-      <div className="section" ref={contactRef}>
+      <div className='section' ref={contactRef}>
         <Contact />
       </div>
       <div>
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,55 +1,92 @@
 import './Projects.css';
-
-
-
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 function Projects() {
+  const animationOffset = 30;
   const lightningRef = () => {
     return (
-    <a href="https://lightningjs.io/" target="_blank" rel="noopener noreferrer">LightningJS</a>
-    )
-  }
+      <a
+        href='https://lightningjs.io/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        LightningJS
+      </a>
+    );
+  };
 
   const xclassTvRef = () => {
     return (
-    <a href="https://www.xclasstv.com/" target="_blank" rel="noopener noreferrer">XClass TV</a>
-    )
-  }
+      <a
+        href='https://www.xclasstv.com/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        XClass TV
+      </a>
+    );
+  };
 
   const githubInterviewRef = () => {
     return (
-    <a href="https://github.com/jbstocker/lightning-interview-question" target="_blank" rel="noopener noreferrer">lightning-interview-question</a>
-    )
-  }
+      <a
+        href='https://github.com/jbstocker/lightning-interview-question'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        lightning-interview-question
+      </a>
+    );
+  };
 
   return (
-    <div className="projects">
-      <div className='project-section'>
-        <img src='/images/xclassTvSearch.webp' alt='Xclass tv' loading='lazy'></img>
-        <div className="text-container">
-          <h1>XClass TV</h1>
-          <p>
-            I currently work as the engineering manager for the {xclassTvRef()} user interface.
-            XClass TV is brand new smart TV user interface, built using the {lightningRef()} framework.
-          </p>
+    <div className='projects'>
+      <AnimationOnScroll
+        animateIn='animate__fadeInLeft'
+        animateOut='animate__fadeOutLeft'
+        offset={animationOffset}
+      >
+        <div className='project-section'>
+          <img
+            src='/images/xclassTvSearch.webp'
+            alt='Xclass tv'
+            loading='lazy'
+          ></img>
+          <div className='text-container'>
+            <h1>XClass TV</h1>
+            <p>
+              I currently work as the engineering manager for the{' '}
+              {xclassTvRef()} user interface. XClass TV is brand new smart TV
+              user interface, built using the {lightningRef()} framework.
+            </p>
+          </div>
         </div>
-      </div>
-      <div className='project-section'>
-      <video autoPlay  muted loop playsInline>
-        <source src="/videos/InterviewQuestions.webm" type="video/webm" />
-        <source src="/videos/InterviewQuestions.mp4" type="video/mp4" />
-      </video>
-      {/* <img src='/images/lightning-interview-success.gif' alt='A basic searchpage' loading='lazy'></img> */}
-        <div className="text-container">
-          <h1>Interview Questions Project</h1>
-          <p>
-            As an engineering manager I have decided to start creating a series of repositories that offer hiring managers
-            a resource to test potential candidates.  
-            The first example({githubInterviewRef()}) is already live and in use today as a {lightningRef()} based challenge.  
-            These challenges are set to be done in a reasonable amount of time and should show off the basic skills of a candidate. 
-          </p>
+      </AnimationOnScroll>
+      <AnimationOnScroll
+        animateIn='animate__fadeInLeft'
+        animateOut='animate__fadeOutLeft'
+        offset={animationOffset}
+      >
+        <div className='project-section'>
+          <video autoPlay muted loop playsInline>
+            <source src='/videos/InterviewQuestions.webm' type='video/webm' />
+            <source src='/videos/InterviewQuestions.mp4' type='video/mp4' />
+          </video>
+          {/* <img src='/images/lightning-interview-success.gif' alt='A basic searchpage' loading='lazy'></img> */}
+          <div className='text-container'>
+            <h1>Interview Questions Project</h1>
+            <p>
+              As an engineering manager I have decided to start creating a
+              series of repositories that offer hiring managers a resource to
+              test potential candidates. The first example(
+              {githubInterviewRef()}) is already live and in use today as a{' '}
+              {lightningRef()} based challenge. These challenges are set to be
+              done in a reasonable amount of time and should show off the basic
+              skills of a candidate.
+            </p>
+          </div>
         </div>
-      </div>
+      </AnimationOnScroll>
     </div>
   );
 }
